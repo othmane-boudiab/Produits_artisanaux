3af98c5d049d92b2bdd1fe536195bd4db313d879
OTHMANE BOUDIAB <mrboudiabothmane@gmail.com>
2021-06-21 11:17:26 +0100

    edits

---
+++ index.js
@@ -0,0 +1,58 @@
+// require important modules
+const express = require('express')
+const bodyParser = require('body-parser')
+
+// require routes
+// const AdminRouter = require('./routes/admin')
+// const categoriesRouter = require('./routes/catégories')
+// const ProductRouter = require('./routes/products')
+// const ClientRouter = require('./routes/client')
+// const CommentaireRouter = require('./routes/commentaire')
+
+
+
+// create our App
+const app = express()
+const port = process.env.PORT || 3000
+
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+
+// require the connection (DB)
+const db = require('./config/database')
+
+
+
+// Home Page
+app.get('/', (req,res) => {
+    res.send('hello')
+  })
+
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api/v1/
+// app.use('/api/v1/', AdminRouter)
+// app.use('/api/v1/', categoriesRouter)
+// app.use('/api/v1/', ClientRouter)
+// app.use('/api/v1/', ProductRouter) // /api/v1/product
+// app.use('/api/v1/', CommentaireRouter)
+
+
+
+// Testing the connection
+db
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.")
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err)
+  })
+
+
+// START THE SERVER
+app.listen(port, () => console.log(`server run on port ${port}`))
