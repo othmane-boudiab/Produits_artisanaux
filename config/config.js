f0e33e0aed7b99434482cf39f29d1b5310fb248f
abdellatif <ouldeabdellatif40@gmail.com>
2021-06-24 13:51:19 +0100

    modifi ou niveau des variable

--- config/config.js
+++ config/config.js
@@ -6,13 +6,13 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     dialect: process.env.DIALECT,
-    logging: false
+   
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     dialect: process.env.DIALECT,
-    logging: false
+    
   }
 }
--- config/database.js
+++ config/database.js
@@ -2,9 +2,11 @@ const Sequelize = require('sequelize')
 const env = process.env.NODE_ENV || 'development'
 const config = require('./config')[env]
 
-module.exports = new Sequelize(
+const connection = new Sequelize(
   config.database,
   config.username,
   config.password,
   config
 )
+
+module.exports = connection
