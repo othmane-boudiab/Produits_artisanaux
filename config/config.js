cc6165109c6d353f99248228dcb97643030cfad5
othmane-boudiab <mrboudiabothmane@gmail.com>
2021-06-28 09:44:57 +0100

    v

--- config/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-module.exports = {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    dialect: process.env.DIALECT,
-}
