4f0e2d80a61b45971e20b907a551c269eca6cbcc
abdellatif <ouldeabdellatif40@gmail.com>
2021-06-30 15:58:07 +0100

    modifications

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,41 +1,39 @@
 
-const category = require('../Models/categoryModel');
-const M_category = require('../Models/categoryModel') 
 
-// display all categories
-exports.all_category=function(req,res){
-    M_category.findAll().
-        then((data) => {
-            res.json(data);
-        })
-        .catch((error) => {
-            console.log("this is the error".error);
-        });
-}
+// const M_category = require('../Models/categoryModel');
 
-// display one category by id
-exports.category=function(req,res){
-    M_category.findById(req.params.id).
-    then((data) => {
-        res.json(data);
-    })
-    .catch((error) => {
-        console.log("this is the error".error);
-    });
-}
+// // display all categories
+// exports.all_category=function(req,res){
+//     M_category.findAll().
+//         then((data) => {
+//             res.json(data);
+//         })
+//         .catch((error) => {
+//             console.log("this is the error".error);
+//         });
+// }
 
-// add a category 
-exports.Add_category=function(req,res){
-   const forme = req.params
+// // display one category by id
+// exports.category=function(req,res){
+//     M_category.findById(req.params.id).
+//     then((data) => {
+//         res.json(data);
+//     })
+//     .catch((error) => {
+//         console.log("this is the error".error);
+//     });
+// }
 
-   M_category.create(forme);
-   res.send(`hi, your category has name ${forme.Name_category} is add to database`)
-   .catch((error)=>{
-       console.log("this is the error".error)
-   })
-}
+// // add a category 
+// exports.Add_category=function(req,res){
+//    const forme = req.params
 
-
+//    M_category.create(forme);
+//    res.send(`hi, your category has name ${forme.Name_category} is add to database`)
+//    .catch((error)=>{
+//        console.log("this is the error".error)
+//    })
+// }
 
 
 
