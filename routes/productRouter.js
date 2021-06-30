2007c61310202ac7890609eda2558a4ce360b932
hajar-co <boudali.hajar@gmail.com>
2021-06-30 08:38:15 +0100

    testing delete

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -20,6 +20,37 @@ router.get('/app/product/:id', (req,res)=>{
         console.log(error);
     })
 })
+router.post('/app/product', (req, res, next)=>{
+    let body = req.body
+    Product.create(body).then(product => {
+        console.log(JSON.stringify(product));
+        res.json(product)
+    }).catch((error)=> {
+        console.log(error);
+    })
+
+})
+router.delete('/app/product/:id', (req,res)=>{
+    const id = req.params.id
+    Product.destroy( { where: {id:id}}).
+    then(()=>{
+        res.json({msg: "the product had been deleted"})
+    }).catch((error)=>{
+        console.log(error);
+    })
+})
+router.put('/app/product/:id', (req,res)=>{
+    Product.update(
+      {title: req.body.title},
+      {where: req.params.bookId}
+    )
+    .then(function(rowsUpdated) {
+      res.json(rowsUpdated)
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+   })
 
 
  module.exports = router
