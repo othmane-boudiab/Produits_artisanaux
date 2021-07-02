a216346b0bf84b69d4b3828f55fd17aaea8cad6f
abdellatif <ouldeabdellatif40@gmail.com>
2021-07-02 12:33:32 +0100

    removed category(add/update/delete)

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -10,16 +10,7 @@ router.get('/categories', controller.all_category)
 //display one category
 router.get('/category/:id',controller.category)
 
-//add category
-router.post('/newCategory', controller.Add_category)
 
-
-// delete a category 
-router.delete('/delete/:id',controller.delete_category)
-
-
-// update a category
-router.put('/edit/:id',controller.updata_category)
 
     
     module.exports = router;
